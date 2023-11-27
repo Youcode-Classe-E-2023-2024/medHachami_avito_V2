a0d5b6642554162ff67e345feaa1868c6d6585e5
MedHachami <moha.hach.9@gmail.com>
2023-11-27 21:18:26 +0100

    Create User.php

---
+++ app/models/User.php
@@ -0,0 +1,89 @@
+<?php
+  class User {
+    private $db;
+
+    public function __construct(){
+      $this->db = new Database;
+    
+    }
+
+    // Regsiter user
+    public function register($data){
+      $this->db->query('INSERT INTO users (name, email, password,city,imgUrl , role) VALUES(:name, :email, :password, :city,:imgUrl , :role)');
+      // Bind values
+      $this->db->bind(':name', $data['name']);
+      $this->db->bind(':email', $data['email']);
+      $this->db->bind(':password', $data['password']);
+      $this->db->bind(':city', $data['city']);
+      $this->db->bind(':imgUrl', $data['image']);
+      $this->db->bind(':role', 'user');
+
+      // Execute
+      if($this->db->execute()){
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    // Login User
+    public function login($email, $password){
+      $this->db->query('SELECT * FROM users WHERE email = :email');
+      $this->db->bind(':email', $email);
+
+      $row = $this->db->single();
+
+      $hashed_password = $row->password;
+      if(password_verify($password, $hashed_password)){
+        return $row;
+      } else {
+        return false;
+      }
+    }
+
+    // Find user by email
+    public function findUserByEmail($email){
+      $this->db->query('SELECT * FROM users WHERE email = :email');
+      // Bind value
+      $this->db->bind(':email', $email);
+
+      $row = $this->db->single();
+
+      // Check row
+      if($this->db->rowCount() > 0){
+        return true;
+      } else {
+        return false;
+      }
+    }
+
+    // Get User by ID
+    public function getUserById($id){
+      $this->db->query('SELECT * FROM users WHERE id = :id');
+      // Bind value
+      $this->db->bind(':id', $id);
+
+      $row = $this->db->single();
+
+      return $row;
+    }
+
+    public function getUers($id){
+      $this->db->query('SELECT * FROM users where id <>:id LIMIT 5;');
+      // Bind value
+      $this->db->bind(':id', $id);
+      $results = $this->db->resultSet();
+
+      return $results;
+    }
+
+    public function get_Users(){
+      $this->db->query('SELECT * FROM users ;');
+      // Bind value
+      
+      $results = $this->db->resultSet();
+
+      return $results;
+    }
+
+  }
