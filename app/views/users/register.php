132ca69d22dc86a4633d4d66b67ae057b4ccc4b2
MedHachami <moha.hach.9@gmail.com>
2023-11-27 21:19:19 +0100

    Create register.php

---
+++ app/views/users/register.php
@@ -0,0 +1,51 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+  <div class="text-secondary">
+    <div class="col-md-4 mx-auto">
+      <div class="card card-body register">
+        <h2>Create An Account</h2>
+        <p>Please fill out this form to register with us</p>
+        <form action="<?php echo URLROOT; ?>/users/register" method="post" enctype="multipart/form-data">
+          <div class="form-group">
+            <label for="name">Name: <sup>*</sup></label>
+            <input type="text" name="name" class="form-control form-control-lg <?php echo (!empty($data['name_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['name']; ?>">
+            <span class="invalid-feedback"><?php echo $data['name_err']; ?></span>
+          </div>
+          <div class="form-group">
+            <label for="email">Email: <sup>*</sup></label>
+            <input type="email" name="email" class="form-control form-control-lg <?php echo (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['email']; ?>">
+            <span class="invalid-feedback"><?php echo $data['email_err']; ?></span>
+          </div>
+          <div class="form-group">
+            <label for="email">City: <sup>*</sup></label>
+            <input type="text" name="city" class="form-control form-control-lg <?php echo (!empty($data['city_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['city']; ?>">
+            <span class="invalid-feedback"><?php echo $data['city_err']; ?></span>
+          </div>
+          <div class="form-group">
+            <label for="email">Image: <sup>*</sup></label>
+            <input type="file" name="image" class="form-control form-control-lg ">
+            <span class="invalid-feedback"><?php echo $data['image_err']; ?></span>
+          </div>
+          <div class="form-group">
+            <label for="password">Password: <sup>*</sup></label>
+            <input type="password" name="password" class="form-control form-control-lg <?php echo (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['password']; ?>">
+            <span class="invalid-feedback"><?php echo $data['password_err']; ?></span>
+          </div>
+          <div class="form-group">
+            <label for="confirm_password">Confirm Password: <sup>*</sup></label>
+            <input type="password" name="confirm_password" class="form-control form-control-lg <?php echo (!empty($data['confirm_password_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['confirm_password']; ?>">
+            <span class="invalid-feedback"><?php echo $data['confirm_password_err']; ?></span>
+          </div>
+
+          <div class="row">
+            <div class="col">
+              <input type="submit" value="Register" class="btn sign-btn btn-block">
+            </div>
+            <div class="col">
+              <a href="<?php echo URLROOT; ?>/users/login" class="btn btn-light btn-block">Have an account? Login</a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
