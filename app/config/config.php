495d764981c33bdfadb0d7e75c7449b86f74f00a
MedHachami <moha.hach.9@gmail.com>
2023-11-27 21:12:37 +0100

    Create config.php

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '');
+  define('DB_NAME', 'avito_DB');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/medHachami_avito_V2');
+  // Site Name
+  define('SITENAME', 'HachamiAVITO');
+?>
